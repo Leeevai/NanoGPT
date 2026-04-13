034a6531c6ecd2b5d2d9d022dfea86a03a137b5a
Leeevai <hassanmohseen22@gmail.com>
2026-04-13 05:49:51 +0200

    fix DDP model handling by using raw_model for optimizer configuration

--- train_gpt2.py
+++ train_gpt2.py
@@ -302,6 +302,7 @@ model = torch.compile(model)
 
 if ddp:
     model = DDP(model, device_ids=[ddp_local_rank])
+raw_model = model.module if ddp else model
 # logits, loss = model(x, targets=y)
 
 
@@ -325,7 +326,7 @@ def get_lr(it):
 
 # optimizer
 # optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4,betas=(0.9, 0.95),eps =1e-8)
-optimizer = model.configure_optimizers(
+optimizer = raw_model.configure_optimizers(
     weight_decay=0.1, learning_rate=3e-4, device=device
 )
 
